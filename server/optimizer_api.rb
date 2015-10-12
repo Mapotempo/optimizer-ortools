5421a933166b7fe37b654cd8c052c4d436a1ad20
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-10-12 14:34:58 +0200

    No constraint on end store

--- server/optimizer_api.rb
+++ server/optimizer_api.rb
@@ -57,7 +57,7 @@ def optimize(capacity, matrix, time_window, rest_window, optimize_time = nil, so
     input.write("\n")
     input.write(matrix.collect{ |a| a.collect{ |b| b.join(" ") }.join(" ") }.join("\n"))
     input.write("\n")
-    input.write((time_window + time_window[-1..-1]).collect{ |a| [a[0] ? a[0]:0, a[1]? a[1]:2147483647, a[2]].join(" ") }.join("\n"))
+    input.write((time_window + [[0, 2147483647, 0]]).collect{ |a| [a[0] ? a[0]:0, a[1]? a[1]:2147483647, a[2]].join(" ") }.join("\n"))
     input.write("\n")
     input.write(rest_window.collect{ |a| [a[0] ? a[0]:0, a[1]? a[1]:2147483647, a[2]].join(" ") }.join("\n"))
     input.write("\n")
