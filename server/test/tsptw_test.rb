0263b4b0181e4c9c1809351d92759c36a4e28ec5
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-12-31 16:19:23 +0100

    Clean test

--- server/test/tsptw_test.rb
+++ server/test/tsptw_test.rb
@@ -7,7 +7,7 @@ class Disjunc < MiniTest::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_negWindow
+  def test_neg_window
     matrix = [
       [[0, 0], [64, 71], [853, 947], [787, 874], [747, 830], [0, 0]],
       [[94, 104], [0, 0], [789, 876], [801, 890], [761, 845], [94, 104]],
@@ -30,7 +30,6 @@ class Disjunc < MiniTest::Unit::TestCase
     post '/0.1/optimize_tsptw', data: {matrix: matrix, time_window: time_window, rest_window: rest_window, capacity: [], optimize_time: 500, soft_upper_bound: 3}.to_json
     assert last_response.ok?
     json = JSON.parse(last_response.body)
-    puts json.inspect
     assert_equal 'ok', json['status']
     assert_equal 6, json['optim'].collect{ |i| Integer(i) }.size
     assert_equal 0, json['optim'].first
@@ -57,17 +56,18 @@ class Disjunc < MiniTest::Unit::TestCase
     rest_window = [
       [600, 1200, 1200]
     ]
+
     post '/0.1/optimize_tsptw', data: {matrix: matrix, time_window: time_window, rest_window: rest_window, capacity: [], optimize_time: 500, soft_upper_bound: 3}.to_json
+
     assert last_response.ok?
     json = JSON.parse(last_response.body)
-    puts json.inspect
     assert_equal 'ok', json['status']
-    assert_equal 7, json['optim'].collect{ |i| Integer(i) }.size
+    assert_equal 7, json['optim'].size
     assert_equal 0, json['optim'].first
-    assert_equal (matrix.size-1), json['optim'].last
+    assert_equal matrix.size - 1, json['optim'].last
   end
 
-  def test_noRest
+  def test_no_rest
     matrix = [
       [[0, 0], [64, 71], [853, 947], [787, 874], [747, 830], [0, 0]],
       [[94, 104], [0, 0], [789, 876], [801, 890], [761, 845], [94, 104]],
@@ -85,13 +85,14 @@ class Disjunc < MiniTest::Unit::TestCase
       [0, 2147483647, 1]
     ]
     rest_window = []
+
     post '/0.1/optimize_tsptw', data: {matrix: matrix, time_window: time_window, rest_window: rest_window, capacity: [], optimize_time: 500, soft_upper_bound: 3}.to_json
+
     assert last_response.ok?
     json = JSON.parse(last_response.body)
-    puts json.inspect
     assert_equal 'ok', json['status']
-    assert_equal 6, json['optim'].collect{ |i| Integer(i) }.size
+    assert_equal 6, json['optim'].size
     assert_equal 0, json['optim'].first
-    assert_equal (matrix.size-1), json['optim'].last
+    assert_equal matrix.size - 1, json['optim'].last
   end
 end
