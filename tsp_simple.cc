f9f91b776684c6f9385ed38241277e845168b724
braktar <gwenael@mapotempo.com>
2016-12-15 10:51:16 +0100

    fix disjunction cost

--- tsp_simple.cc
+++ tsp_simple.cc
@@ -41,8 +41,7 @@ namespace operations_research {
 
 void TWBuilder(const TSPTWDataDT &data, RoutingModel &routing, Solver *solver, int64 size, int64 min_start, bool loop_route, bool unique_configuration) {
   const int size_vehicles = data.Vehicles().size();
-  int64 disjunction_cost = std::min((int64)std::pow(2, 52), ((2 * data.MaxTime() + data.MaxServiceTime()) * data.MaxTimeCost() + 2 * data.MaxDistance() * data.MaxDistanceCost()) * (data.SizeMatrix() * data.SizeMatrix() + data.TWsCounter() * data.TWsCounter()) * (data.SizeRest() > 0 ? 50000: 1));
-  disjunction_cost = std::pow(2, 52);
+  int64 disjunction_cost = std::min((int64)std::pow(2, 52), ((2 * data.MaxTime() + data.MaxServiceTime()) * data.MaxTimeCost() + 2 * data.MaxDistance() * data.MaxDistanceCost()) * (data.SizeMatrix() * data.SizeMatrix() + data.TWsCounter() * data.TWsCounter()) * size_vehicles * size * (data.SizeRest() > 0 && !loop_route && !unique_configuration? size : 1));
   for (RoutingModel::NodeIndex i(0); i < size; ++i) {
     int64 const first_ready = data.ReadyTime(i);
     int64 const first_due = data.DueTime(i);
