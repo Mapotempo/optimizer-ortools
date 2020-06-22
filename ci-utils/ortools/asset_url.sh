e70bd80663230471aa0d080cbaa2bb246e4e64e4
Braktar <gwenael@mapotempo.com>
2020-06-22 15:23:20 +0200

    OR version as variable

--- ci-utils/ortools/asset_url.sh
+++ ci-utils/ortools/asset_url.sh
@@ -4,6 +4,9 @@ case $ORTOOLS_VERSION in
   'v7.1')
     export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.1/or-tools_debian-9_v7.1.6720.tar.gz"
     ;;
+  'v7.4')
+    export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.4/or-tools_debian-9_v7.4.7247.tar.gz"
+    ;;
   'v7.5')
     export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.5/or-tools_debian-10_v7.5.7466.tar.gz"
     ;;
