a8ea1b1d0feef35b2c43462c0fb18c54eb341621
Alain ANDRE <wordsbybird@alain-andre.fr>
2023-01-17 18:47:35 +0100

    Make build available on arm64

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -13,25 +13,7 @@ fi
 
 IMAGE_NAME=${REGISTRY}/mapotempo-ce/optimizer-ortools:${TAG}
 
-case $ORTOOLS_VERSION in
-  'v7.1')
-    export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.1/or-tools_debian-9_v7.1.6720.tar.gz"
-    ;;
-  'v7.4')
-    export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.4/or-tools_debian-9_v7.4.7247.tar.gz"
-    ;;
-  'v7.5')
-    export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.5/or-tools_debian-10_v7.5.7466.tar.gz"
-    ;;
-  'v7.8')
-   export ORTOOLS_URL="https://github.com/google/or-tools/releases/download/v7.8/or-tools_debian-10_v7.8.7959.tar.gz"
-    ;;
-  *)
-    echo "Unknown OR-Tools version"
-esac
-
-echo "Download asset at ${ORTOOLS_URL}"
-docker build --build-arg RUBY_VERSION="2.6" --build-arg ORTOOLS_URL=${ORTOOLS_URL} -f ./Dockerfile -t "${IMAGE_NAME}" .
+docker build --build-arg RUBY_VERSION="2.5" --build-arg ORTOOLS_VERSION="${ORTOOLS_VERSION}" -f ./Dockerfile -t "${IMAGE_NAME}" .
 docker run -d --name optimizer -t "${IMAGE_NAME}"
 docker exec -i optimizer bash -c "LD_LIBRARY_PATH=/srv/or-tools/lib/ /srv/optimizer-ortools/tsp_simple -time_limit_in_ms 500 -intermediate_solutions -instance_file '/srv/optimizer-ortools/data/test_ortools_single_route_with_route_order' -solution_file '/tmp/optimize-or-tools-output'"
 
--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,31 @@ ARG ARCHITECTURE
 
 FROM ${ARCHITECTURE}ruby:${RUBY_VERSION}
 
-ARG ORTOOLS_URL=${ORTOOLS_URL}
+ARG ORTOOLS_VERSION=${ORTOOLS_VERSION}
 
 LABEL maintainer="Mapotempo <tech@mapotempo.com>"
 
-WORKDIR /srv/
-
 RUN apt-get update > /dev/null && \
-  apt-get -y install git wget pkg-config build-essential cmake autoconf libtool zlib1g-dev lsb-release > /dev/null
+  apt install -y git build-essential cmake swig lsb-release python3-pip autoconf libtool zlib1g-dev > /dev/null && \
+  git clone -b ${ORTOOLS_VERSION} https://github.com/google/or-tools /srv/or-tools
 
-ADD . /srv/or-tools
+# tmp until ruby image have the good cmake version
+RUN curl 'https://github.com/Kitware/CMake/releases/download/v3.15.7/cmake-3.15.7.tar.gz' -s -L -R -o cmake.tar.gz && \
+  tar -zxvf cmake.tar.gz && \
+  cd cmake-3.15.7 && \
+  ./bootstrap && \
+  make && \
+  make install && \
+  cd .. && \
+  rm -R cmake-3.15.7 cmake.tar.gz
 
-RUN wget -qO- $ORTOOLS_URL | tar xz --strip-components=1 -C /srv/or-tools
+WORKDIR /srv/or-tools
+RUN /usr/local/bin/cmake -S . -B build -DBUILD_DEPS=ON -DBUILD_PYTHON=ON && \
+  /usr/local/bin/cmake --build build --config Release --target install -v
 
 ADD . /srv/optimizer-ortools
 
 WORKDIR /srv/optimizer-ortools
-RUN make tsp_simple
+RUN make tsp_simple && \
+  cd .. && \
+  rm -R /srv/or-tools
